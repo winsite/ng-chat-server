11dfe71eb8f0eb1653ad9bb99bb02355994e65f7
Lukáš Janeček <xjacka@gmail.com>
2016-05-30 14:57:27 +0200

    changed port

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
 const app = require('./app.js');
-express().use(app).listen(8080, function() {
-	console.log('listening port 8080');
+express().use(app).listen(8008, function() {
+	console.log('listening port 8008');
 });
